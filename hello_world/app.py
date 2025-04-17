87a770f4cd95707820fb2920ddf256bf84d5454a
Nripendra101 <nripendra972000@gmail.com>
2025-04-17 14:49:11 +0530

    update lambda function and requirements

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,41 +1,16 @@
 import json
-
-# import requests
+import pandas as pd
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-    a=3
-    b=6
-    c=a+b
-    print(f"the sum of {a} and {b} is {c}")
+    count=event['count']
+    data=[]
+    for i in range(count):
+        data.append(i*10)
+    df=pd.dataframe(data,Columns=['Numbers'])
+    print('data:')
+    print(df)
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
